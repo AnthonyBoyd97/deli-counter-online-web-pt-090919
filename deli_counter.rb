36a3217a1d1395c3e19b7735ce92020699d82a35
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:54:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,17 @@ def line(deli)
     print "\n"
   end
 end
+
+def take_a_number(line,person)
+  deli<<person
+  puts "Welcome, #{person}. You are number #{line.length+1} in line."
+end
+
+def now_serving(line)
+  if line.length==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  end
+end
