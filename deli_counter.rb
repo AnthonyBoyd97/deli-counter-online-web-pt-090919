0b8a551ea7d6239b00920f7a463c3e494d3685dd
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:45:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+deli=[]
+
+def line(deli)
+  line=[]
+  if deli.length==0
+    puts "The line is currently empty."
+    return
+  else
+    print "The line is currently"
+    deli.each_with_index do |name,position|
+    print " #{position+1}. #{name}"
+    end
+  end
+  print "\n"
+end
+  
